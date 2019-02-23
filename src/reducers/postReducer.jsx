2d1bc84e3f203fa38913910dd753b6dfbe1e5241
Venkat <venkat@info-ways.com>
2019-02-24 01:18:07 +0530

    React-with-Redux Initial Commit
    React-with-Redux Initial Commit
    Redux Implementaion

---
+++ src/reducers/postReducer.jsx
@@ -0,0 +1,23 @@
+import { FETCH_POSTS, NEW_POSTS } from "../actions/types"
+
+const initialState = {
+    items: [],
+    item: {}
+}
+
+export default (state = initialState, { type, payload }) => {
+    switch (type) {
+        case FETCH_POSTS:
+            return {
+                ...state,
+                items: payload
+            }
+        case NEW_POSTS:
+            return {
+                ...state,
+                item: payload
+            }
+        default:
+            return state
+    }
+}
